1d9b485b001db4339e9049d3599c643684463210
tommy-tommy-tom <105212576+tom-mcivor@users.noreply.github.com>
2023-03-28 06:01:58 +1300

    hello

---
+++ server/src/index.ts
@@ -0,0 +1,29 @@
+import express, { Request, Response } from 'express'
+import mongoose from 'mongoose'
+import Deck from './models/Deck'
+
+const PORT = 5000
+
+const app = express()
+
+app.use(express.json())
+
+app.post("/decks", async (req: Request, res: Response) => {
+  const newDeck = new Deck({
+    title: req.body.title,
+  })
+  const createdDeck = await newDeck.save()
+  res.json(createdDeck)
+})
+
+const db = mongoose.connect('mongodb+srv://ttmcivor:A9VQj5sZukJYOLRc@cluster0.qdsvupt.mongodb.net/?retryWrites=true&w=majority')
+.then(() => {
+  console.log(`listening on port ${PORT}`);
+  app.listen(PORT)
+})
+
+
+
+
+
+
